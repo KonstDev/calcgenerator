65e2651bb40d181cf22a567c538dbdd9d8ca2fa2
KonstDev <46225922+KonstDev@users.noreply.github.com>
2020-06-15 12:07:14 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ofstream write ("gencalc.cpp");
+    //
+    //cout << "Hello world!" << endl;
+    write << "#include <iostream>" << endl;
+    write << endl;
+    write << "using namespace std;" << endl << endl;
+    write << "int main()" << endl << "{" << endl;
+    write << "int a1 = 0, a2 = 0;" << endl;
+    write << "cin >> a1 >> a2;" << endl;
+    for (int i = 0; i < 1001; i++)
+        for (int j = 0; j < 1001; j++)
+            {
+                write << "if (a1 == " <<  i << " && a2 == " << j << ");"<< endl;
+                write << "        cout << " << i + j << ";" << endl;
+            }
+
+    write << "return 0;" << endl;
+    write << "}";
+    return 0;
+}
